799ca42cd11b3874480532da87f556b272fcb3b4
NotArt <noterisarthur42@gmail.com>
2024-11-18 22:25:13 +0100

    + | Two little functions

--- pipex.c
+++ pipex.c
@@ -6,22 +6,52 @@
 /*   By: anoteris <noterisarthur42@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 21:15:37 by anoteris          #+#    #+#             */
-/*   Updated: 2024/11/18 21:18:27 by anoteris         ###   ########.fr       */
+/*   Updated: 2024/11/18 22:24:30 by anoteris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-extern char **environ ;
+extern char	**environ ;
 
-char	**get_paths()
+char	**get_paths(void)
 {
-	char **paths ;
-	int i = -1;
-	while (environ[++i] && strncmp(environ[i], "PATH=", 5)) ;
+	char	**paths ;
+	int		i ;
+
+	paths = NULL ;
+	i = 0;
+	while (environ[i] && strncmp(environ[i], "PATH=", 5))
+		i++;
 	if (environ[i])
-		paths = ft_split(&environ[i][5], ':') ;
-	else
-		paths = NULL;
+		paths = ft_split(&environ[i][5], ':');
 	return (paths);
+}
+
+// TODO:	think of the error case when using this function
+//			if the cmd isn't found in any of the paths, and null is returned
+char	*add_path_to_cmd(char *cmd)
+{
+	char	**paths ;
+	char	*res ;
+	int		i ;
+
+	paths = get_paths();
+	i = -1 ;
+	while (paths && paths[++i])
+	{
+		res = ft_calloc(ft_strlen(paths[i]) + ft_strlen(cmd) + 2, 1);
+		ft_strlcpy(res, paths[i], ft_strlen(paths[i]) + 1);
+		ft_strlcat(res, "/", ft_strlen(res) + 2);
+		ft_strlcat(res, cmd, ft_strlen(res) + ft_strlen(cmd) + 1);
+		if (!access(res, F_OK))
+		{
+			free_str_array(paths);
+			return (res);
+		}
+		else
+			free (res);
+	}
+	free_str_array(paths);
+	return (NULL);
 }
--- pipex_utils.c
+++ pipex_utils.c
@@ -1,22 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anoteris <noterisarthur42@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 21:16:50 by anoteris          #+#    #+#             */
-/*   Updated: 2024/11/18 21:47:39 by anoteris         ###   ########.fr       */
+/*   Created: 2024/11/18 21:25:10 by anoteris          #+#    #+#             */
+/*   Updated: 2024/11/18 22:23:40 by anoteris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "pipex.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-# include "libft/libft.h"
+void	free_str_array(char **to_free)
+{
+	int	i ;
 
-void	free_str_array(char **to_free);
-
-#endif
+	i = -1 ;
+	while (to_free && to_free[++i])
+		free(to_free[i]);
+	free(to_free);
+}
